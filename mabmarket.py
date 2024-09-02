d2c9ad0d3c233135d0052d89078f62b0d7cd625a
DuangkamonBoonma <poolluck555@gmail.com>
2024-09-03 00:27:15 +0700

    Update fishing.py

--- mabmarket.py
+++ mabmarket.py
@@ -2,31 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-book_url = "https://www.mebmarket.com/"
+dof_url = "https://www4.fisheries.go.th/dof/main"
 
 BASE_HEADERS = {
-    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
     "accept":"*/*",
+    "accept-language":"en-US,en;q=0.9",
     "accept-encoding":"gzip, deflate, br, zstd",
-    "accept-language":"th-TH,th;q=0.9,pt;q=0.8",
 }
 
-response = requests.get(book_url, headers=BASE_HEADERS)
+response = requests.get(dof_url, headers=BASE_HEADERS)
+
 soup = BeautifulSoup(response.text, "html.parser")
-content = soup.find_all(class_ = "button_book_list buy_button btn-add-cart-book-309268 ")
-content = str(content)
 
-re_values = r'value="฿([^"]+)"'
-re_prices = r'price">([^<]+)</p>'
+content = soup.find_all('a') 
 
-value_list = re.findall(re_values, content)
-price_list = re.findall(re_prices, content)
+title_list = []
 
-print("Values found:")
-print(value_list)
-print("Prices found:")
-print(price_list)
+for element in content:
+    title = element.get('title')
+    
+    if title:
+        title_list.append(title)
 
-with open('output.txt', 'w') as f:
-    for value, price in zip(value_list, price_list):
-        f.write(f"{value}\t{price}\n")
+print(title_list)
